68b2dd25273c4d4961738546b2722c184b690d8c
Volodymyr Maksymiuk <Volodymyr.Maksymiuk@Alvaria.com>
2024-05-17 17:43:50 +0300

    about text added

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,8 @@ import SectionWrapper from '../hoc/SectionWrapper';
 
 export function About() {
   return (
-    <section className={`relative w-full h-[50vh] mx-auto`}>
+    <section className={`relative w-full mx-auto h-[60vh] xl:h-[60vh] sm:h-[100vh] xs:h-[80vh] xxs:h-[120vh] `} // xl:h-[60vh] sm:h-[100vh] xs:h-[80vh] xxs:h-[120vh]
+    >
       <div
         className={`absolute inset-0 top-[60px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -14,12 +15,30 @@ export function About() {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText}`}> Hello and welcome, {/* I'm <span className='text-[#915EFF]'>VM</span> */} </h1>
+          <h1 className={`${styles.heroHeadText}`}> Hello and welcome! {/* I'm <span className='text-[#915EFF]'>VM</span> */} </h1>
           <p className={`${styles.heroSubText} mt-2`}>
-            Here is something that you probably want to know about me before scrolling down
-            Please, take a look at my professional experience  <br className='sm:block hidden' />
-            while I'm floating here in the sky
+            Here are a few things you should know about me before scrolling down:
           </p>
+            <br className='sm:block hidden' />
+          <ul className={`list-disc ml-5 ${styles.aboutPointText}`}>
+          {/* <ul className={`list-disc ml-5 text-text3rd text-[20px] xxs:text-[16px]`}> */}
+              <li >
+                My name is <span className='text-text2nd'>Volodymyr</span>, and I'm a software developer.
+              </li>
+              <li>
+                With almost <span className='text-text2nd'>3 years</span> of commercial experience in the IT industry, I have a passion for developing and learning new things.
+              </li>
+              <li>
+                During this time, I've had the opportunity to work on several projects with varying levels of complexity, both as a dedicated <span className='text-text2nd'>backend</span> developer within a cross-functional team and as a <span className='text-text2nd'>full-stack</span> developer.
+              </li>
+              <li>
+                I've guided features from initial discussions with customers to delivery for end-users.
+              </li>
+              <li>
+                Last but not least, I have more than 5 years of expertise as a mechanical design engineer, with various tasks and projects completed.
+              </li>
+            </ul>
+          
         </div>
       </div>
     </section>
--- src/styles.ts
+++ src/styles.ts
@@ -4,10 +4,11 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "text-text2nd font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+    "text-text2nd font-black lg:text-[70px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
   heroSubText:
-    "text-text3rd font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
-
+    "text-text3rd font-medium lg:text-[25px] sm:text-[20px] xs:text-[16px] text-[16px] lg:leading-[35px]",
+  aboutPointText:
+    "text-text3rd font-medium lg:text-[25px] sm:text-[20px] xs:text-[16px] text-[16px] lg:leading-[35px]",
   sectionHeadText:
     "text-text2nd font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
